c4e3da606ce473a23792f4e371f1d198132c4c26
Grant Skaggs <grant.skaggs@outlook.com>
2020-09-02 22:39:59 -0500

    Add dp soln to edit dist problem

--- leet_code/edit-dist/src/Solution.java
+++ leet_code/edit-dist/src/Solution.java
@@ -1,62 +1,33 @@
-import java.util.*;
-
 class Solution {
-    private class Node {
-        public int dist;
-        public String word;
-        public Node(String str, int d) {
-            dist = d;
-            word = str;
-        }
-    }
+    private int[][] dp;
     
-    private Set<String> seen = new HashSet<String>();
-    private String target;
-
-    private List<String> nextStrings(String word) {
-        List<String> res = new ArrayList<String>();
-        int len = word.length();
-        for (int i = 0; i < len; i++) {
-            res.add(word.substring(0,i) + word.substring(i+1, len));
-            for (int j = 0; j < target.length(); j++) {
-                char c = target.toCharArray()[j];
-                res.add(word.substring(0,i) + c + word.substring(i+1, len));
-                res.add(word.substring(0,i) + c + word.substring(i, len));
-            }
-        }
+    private int editDist(String a, String b, int m, int n) {
+        if (m == 0 || n == 0) 
+            return Math.max(m, n);
         
-        for (int j = 0; j < target.length(); j++) {
-            char c = target.toCharArray()[j];
-            res.add(word + c);
-        }
+        if (dp[m][n] != -1) 
+            return dp[m][n];
         
-        return res;
+        if (a.charAt(m-1) == b.charAt(n-1)) 
+            return editDist(a, b, m-1, n-1);
+        
+        dp[m][n] = 1 + Math.min(
+            Math.min(editDist(a, b, m-1, n), editDist(a, b, m, n-1)),
+            editDist(a, b, m-1, n-1)
+        );
+        
+        return dp[m][n];
     }
-    
     public int minDistance(String word1, String word2) {
-        Queue<Node> q = new LinkedList<Node>();
-        target = word2;
-        Node firstNode = new Node(word1, 0);
-        q.add(firstNode);
+        int M = word1.length(), N = word2.length();
+        dp = new int[M+1][N+1];
         
-        // BFS
-        while (!q.isEmpty()) {
-            Node cur = q.poll();
-            
-            System.out.println(cur.word);
-            if (seen.contains(cur.word)) 
-                continue;
-            seen.add(cur.word);
-            
-            if (word2.equals(cur.word)) 
-                return cur.dist;
-            
-            List<String> next = nextStrings(cur.word);
-            
-            for (String nextString : next) {
-                q.add(new Node(nextString, cur.dist+1));
+        for (int i = 0; i < M+1; i++) {
+            for (int j = 0; j < N+1; j++) {
+                dp[i][j] = -1;
             }
         }
-        return -1;
+        
+        return editDist(word1, word2, M, N);
     }
 }
