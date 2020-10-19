d59e7df227414f0dd6a0bb516763283c79b73326
Grant Skaggs <grant.skaggs@outlook.com>
2020-10-19 10:33:56 -0500

    Compute next permutation of array

---
+++ elements_of_programming_interviews/nextPermutation/Solution.java
@@ -0,0 +1,37 @@
+public class Solution {
+    private static void swap(int[] A, int i, int j) {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+
+    // Reverse A from idx to the end
+    private static void reverse(int[] A, int idx) {
+        for (int i = idx; i < (A.length + idx) / 2; i++) {
+            swap(A, i, A.length - (i - idx) - 1);
+        }
+    }
+
+    public static boolean nextPermutation(int[] A) {
+        int i = A.length-2;
+        while (i >= 0 && A[i] < A[i+1]) i--;
+        if (i < 0) return false; 
+
+        int j = A.length-1;
+        while (j > i && A[j] >= A[i]) j--;
+
+        swap(A, i, j);
+        reverse(A, i+1);
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] A = {4, 3, 2, 1};
+        while(Solution.nextPermutation(A)) {
+            for(int i = 0; i < A.length; i++) {
+                System.out.print(A[i] + " ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
