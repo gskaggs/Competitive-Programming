b01f512328febd41c5b44af2acd57479007dd27d
Grant Skaggs <grant.skaggs@outlook.com>
2020-12-11 14:16:52 -0600

    Remove big files

--- contests/2-28-2020-Contest/TakingInventory/ac/grant.py
+++
@@ -1,8 +0,0 @@
-B, R, C, A = map(int, input().split())
-
-if B >= 10**6 or R >= 10**6 or C >= 10**6 or A >= 10**6:
-    print("error")
-    
-total = B * 1440 + R * 590 + C * 224 + A * 130
-
-print(total // 2000)
